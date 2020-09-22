9b4055717d53393dcd0267aea398bcb272573829
NagaiMatsuge <Komilovboburweb@gmail.com>
2020-09-22 21:06:50 +0500

    Initial Commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-blue-500">
+    <div class="nav__bar_right">
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="Portfolio">Portfolio</router-link>
+        </li>
+        <li>
+          <router-link to="About">About</router-link>
+        </li>
+        <li>
+          <router-link to="Projects">Projects</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="nav__bar_left">
+      <ul>
+        <li>Github</li>
+        <li>Instagram</li>
+        <li>Telegram</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Projects.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>Personal Projects</h3>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
